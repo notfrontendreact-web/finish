9690ae9cff9b6b35b4b6b1af1f9517d5403a2e8d
notfrontendreact <notfrontendreact@gmail.com>
2026-07-22 02:10:53 +0330

    fix: config for github pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-   /** @type {import('next').NextConfig} */
-   const nextConfig = {
-     output: 'export', // تبدیل به سایت استاتیک
-     images: {
-       unoptimized: true, // غیرفعال کردن بهینه‌سازی تصاویر
-     },
-     basePath: '/storyshop', // نام ریپازیتوری گیت‌هاب شما
-   };
-
-   module.exports = nextConfig;
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/storyshop',
+}
+module.exports = nextConfig
