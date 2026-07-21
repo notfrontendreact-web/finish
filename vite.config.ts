833b8e1abab60f602a13c4d06f854ed9e4430c79
notfrontendreact <notfrontendreact@gmail.com>
2026-07-22 03:01:03 +0330

    fix github pages path

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/milad/',
+  base: '/finish/',
 
   plugins: [
-    react(),
+    react()
   ],
 
   build: {
-    outDir: 'dist',
-  },
+    outDir: 'dist'
+  }
 })
