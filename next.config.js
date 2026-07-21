f91a12e9fbb8d9b2a8dd3acdc7e66986e966a9ee
notfrontendreact <notfrontendreact@gmail.com>
2026-07-22 02:48:20 +0330

    fix vite github pages deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: '/storyshop',
-}
-module.exports = nextConfig
+  output: "export",
+};
+
+export default nextConfig;
